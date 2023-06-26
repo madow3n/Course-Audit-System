45b6c485b63b6c3db844e26e9af3abe551019e48
madow3n <phongkang123@gmail.com>
2023-06-26 16:31:50 +0700

    pre-oral update

--- resources/views/users/view.blade.php
+++ resources/views/users/view.blade.php
@@ -16,38 +16,92 @@
                         @foreach ($semester as $sem)
                             <h5>Semester {{ $sem }}</h5>
                             <table class="table">
-                                <thead>
+
+                                <thead class="thead-light">
+
                                     <tr>
-                                        <th>Code</th>
-                                        <th>Course Name</th>
-                                        <th>Credit</th>
-                                        <th>Type(s)</th>
-                                        <th>Grades</th>
+                                        <th style="width: 50px">Code</th>
+                                        <th style="width: 120px">Course Name</th>
+                                        <th style="width: 30px">Credit</th>
+                                        <th style="width: 120px">Type(s)</th>
+                                        <th style="width: 80px">Grades</th>
                                         </th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @foreach ($courseGroups[$sem] ?? [] as $course)
-                                        <tr>
-                                            <td style="width: 50px">{{ $course->code_name }}</td>
-                                            <td style="width: 140px">{{ $course->name }}</td>
-                                            <td style="width: 30px">{{ $course->credit }}</td>
-                                            <td style="width: 120px">
-                                                @foreach ($course->types as $type)
-                                                    {{ $type->name }}
-                                                    @if (!$loop->last)
-                                                        /
-                                                    @endif
-                                                @endforeach
-                                            </td style="width: 100px">
-                                            @php
-                                                $userGrade = $user->courses->where('id', $course->id)->first()?->grades;
-                                                
-                                            @endphp
 
-                                            <td style="width: 100px">{{ $userGrade?->letter_grade }}</td>
-                                        </tr>
+                                    @foreach ($courseGroups[$sem] ?? [] as $course)
+                                        @php
+                                            $shouldRender = true;
+                                            
+                                            foreach ($semester as $s) {
+                                                if ($s !== $sem) {
+                                                    $_courses = $courseGroups[$s] ?? [];
+                                            
+                                                    foreach ($_courses as $_course) {
+                                                        if ($course->id != $_course->id) {
+                                                            continue;
+                                                        }
+                                            
+                                                        $_userGrade = $user
+                                                            ->courses()
+                                                            ->wherePivot('semester', $s)
+                                                            ->where('courses.id', $_course->id)
+                                                            ->first()?->grades;
+                                            
+                                                        if ($_userGrade) {
+                                                            // semster pseng mean grade
+                                            
+                                                            $shouldRender = false;
+                                            
+                                                            break;
+                                                        }
+                                                    }
+                                            
+                                                    if (!$shouldRender) {
+                                                        break;
+                                                    }
+                                                }
+                                            }
+                                            
+                                            if ($shouldRender) {
+                                                $userGrade = $user
+                                                    ->courses()
+                                                    ->wherePivot('semester', $sem)
+                                                    ->where('courses.id', $course->id)
+                                                    ->first()?->grades;
+                                            
+                                                $canEditGrade = ($user->year_level == 'Freshman' && ($sem == 1 || $sem == 2)) || ($user->year_level == 'Sophomore' && ($sem == 1 || $sem == 2 || $sem == 3 || $sem == 4)) || ($user->year_level == 'Junior' && ($sem == 1 || $sem == 2 || $sem == 3 || $sem == 4 || $sem == 5 || $sem == 6)) || ($user->year_level == 'Senior' && ($sem == 1 || $sem == 2 || $sem == 3 || $sem == 4 || $sem == 5 || $sem == 6 || $sem == 7 || $sem == 8));
+                                            }
+                                            
+                                        @endphp
+                                        @if ($course)
+                                            @if ($shouldRender)
+                                                <tr>
+                                                    <td style="width: 50px">{{ $course->code_name }}</td>
+                                                    <td style="width: 140px">{{ $course->name }}</td>
+                                                    <td style="width: 30px">{{ $course->credit }}</td>
+                                                    <td style="width: 120px">
+                                                        <p>
+                                                            @foreach ($course->types as $type)
+                                                                {{ $type->name }}
+                                                                @if (!$loop->last)
+                                                                    /
+                                                                @endif
+                                                            @endforeach
+                                                        </p>
+                                                    </td>
+
+
+                                                    <td style="width: 100px">
+                                                        {{ $userGrade?->letter_grade ?? '-' }}
+                                                    </td>
+
+                                                </tr>
+                                            @endif
+                                        @endif
                                     @endforeach
+
 
                             </table>
                             <br />
